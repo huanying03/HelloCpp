3785adb003fa2a978ea3a9f5cbc3edad4a169186
huanying03 <498986095@qq.com>
2015-12-28 10:57:35 +0000

    edit on the mobile

--- while_continue.cpp
+++ while_continue.cpp
@@ -1,13 +1,18 @@
 #include <stdio.h>
 int main()
 {
+    printf("test : continue in while, if (i==5) continue;\n");
+    printf("add a new line under baidu explorer\n");
+    printf("if new i=6,continue.  edit on the mobile.\n");//u")
     int i=0;
     while (i<10)
     {
         printf("i=%d\n",i);
         i++;
-        if (i==5) continue;
-        printf("-----------\n");
+        if (i==6) continue;//55 
+        //／/+
+        //if (i＝6) continue;
+        printf("-----------new i=%d\n",i);
     }
     return 0;
 }
